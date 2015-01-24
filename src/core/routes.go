537071b6274f12ce3c39e9493ddc6ed9ea714b11
Jackman <jackman@jackmanlabs.com>
2015-01-24 15:38:54 -0700

    removing some extraneous bits.

--- src/core/routes.go
+++ src/core/routes.go
@@ -1,16 +1,10 @@
 package core
 
 import (
-	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func init() {
-
-	rmux := mux.NewRouter()
-
 	// I expect someone else on the team to know what routing scheme we want to use.
-	http.Handle("/jackman/", rmux)
-
-	rmux.HandleFunc("/submit/", submitHandler)
+	http.HandleFunc("/submit/", submitHandler)
 }
